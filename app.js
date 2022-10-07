7b2d4da7af295eb345982d05d217c8081eaf3104
mimoon5860 <mahmodulislammoon@gmail.com>
2022-10-08 00:16:21 +0600

    apollo server added

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const cors = require('cors');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express();
+const typeDefs = gql`
+    type Query{
+        hello:String
+        nani:String
+    }
+`;
 
-app.get('/app', (req, res) => {
-    res.send('hello boss')
-})
+const resolvers = {
+    Query: {
+        hello: () => "Hello world",
+        nani: () => "Hello nani",
+    }
+}
 
 
-app.listen(4000, () => {
-    console.log("App is running at port 4000")
-})
+
+
+
+const startServer = async () => {
+    const app = express();
+    const apolloServer = new ApolloServer({
+        typeDefs, resolvers
+    })
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    app.use((req, res) => {
+        res.send("Hey bro server is running...");
+    })
+    app.listen(4000, () => {
+        console.log("App is running at port 4000")
+    })
+}
+
+
+startServer();
